6d197e52e63e06d2f04039bf6304e79c8e5eda22
Alex Howard <thezanke@gmail.com>
2017-12-09 23:34:53 -0500

    starting d5c2

--- 2017/day5.js
+++ 2017/day5.js
@@ -1,13 +1,18 @@
 // @flow
-const countJumps = (instructions: number[]): number => {
+const countJumps = (
+  instructions: number[],
+  incrementer: (offset: number) => number = () => 1
+): number => {
   let jumps = 0;
-  let index = 0;
+  let offset = 0;
 
-  while (index >= 0 && index < instructions.length) {
-    const current = index;
+  while (offset >= 0 && offset < instructions.length) {
+    const currentOffset = offset;
+    const currentValue = instructions[currentOffset];
+
     jumps++;
-    index += instructions[current];
-    instructions[current]++;
+    offset += currentValue;
+    instructions[currentOffset] += incrementer(currentValue);
   }
 
   return jumps;
