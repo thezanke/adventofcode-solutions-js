d13727fef0cd57764f1fa69308f4b2ff110ba76b
Alex Howard <thezanke@gmail.com>
2017-12-19 14:45:23 -0500

    Day7 flow updates

--- 2017/day7.js
+++ 2017/day7.js
@@ -1,37 +1,41 @@
 // @flow
-const PROGRAM_TEST = /^([a-z]+) \((\d+)\)(?: -> ((?:[a-z]+(?:, )?)+))?/;
-
-const processRow = (row: string): { name: string, weight: number, children: string[] } => {
-  const [_, name, weightStr, childrenStr] = PROGRAM_TEST.exec(row);
-  const weight = parseInt(weightStr, 10);
-  const children = childrenStr && childrenStr.split(', ');
-  return { name, weight, children };
+type Branch = {
+  name: string,
+  weight: number,
+  children: ?(Branch[]),
+  childNames: ?(string[])
 };
 
-const sumBranch = branch => {
-  let total = branch.weight;
-  if (children) {
-    
-  }
-}
+const PROGRAM_TEST = /^([a-z]+) \((\d+)\)(?: -> ((?:[a-z]+(?:, )?)+))?/;
 
-const buildTree = (_input: string[]) => {
-  const input = _input.map(processRow);
-  const allChildren = [].concat(...input.map(r => r.children).filter(Boolean));
-  const hash = input.reduce((obj, row) => {
+const processRow = (row: string): Branch => {
+  const [_, name, weightStr, childrenStr] = PROGRAM_TEST.exec(row);
+  const weight = parseInt(weightStr, 10);
+  const childNames = childrenStr && childrenStr.split(', ');
+  return { name, weight, children: null, childNames };
+};
+
+const sumBranch = (branch: Branch) => {
+  let total = branch.weight;
+  if (branch.children) {
+    total += branch.children.map(sumBranch).reduce((a, b) => a + b, 0);
+  }
+  return total;
+};
+
+const buildTree = (input: string[]): ?Branch => {
+  const branches: Branch[] = input.map(processRow);
+  const allChildNames: string[] = [].concat(...branches.map(r => r.childNames).filter(Boolean));
+  const hash: { [string]: Branch } = branches.reduce((obj, row) => {
     obj[row.name] = row;
     return obj;
   }, {});
 
-  input.forEach(row => {
-    if (row.children) {
-      hash[row.name].children = row.children.map(childName => hash[childName]);
-    }
+  branches.forEach(({ name, childNames }) => {
+    if (childNames) hash[name].children = childNames.map((childName: string) => hash[childName]);
   });
 
-  const root = input.find(r => !allChildren.includes(r.name));
-
-  return root;
+  return branches.find(branch => !allChildNames.includes(branch.name));
 };
 
-module.exports = { buildTree };
+module.exports = { buildTree, sumBranch };
--- 2017/day7.test.js
+++ 2017/day7.test.js
@@ -1,4 +1,4 @@
-const { buildTree } = require('./day7');
+const { sumBranch, buildTree } = require('./day7');
 const { EXAMPLE_INPUT, CHALLENGE_INPUT } = require('./day7.input');
 
 test('build tree for example input', () => {
@@ -9,4 +9,9 @@ test('build tree for example input', () => {
 test('build tree for challenge input', () => {
   const root = buildTree(CHALLENGE_INPUT);
   expect(root.name).toEqual('vvsvez');
+});
+
+test('sum branch for example input', () => {
+  const root = buildTree(EXAMPLE_INPUT);
+  expect(sumBranch(root)).toEqual(778);
 });
