de45faa676b18ac73d049707c9d6fc3f96834e46
Alex Howard <thezanke@gmail.com>
2017-12-22 15:39:57 -0500

    Finally finished day 7

--- 2017/day7.js
+++ 2017/day7.js
@@ -2,9 +2,15 @@
 type Branch = {
   name: string,
   weight: number,
-  children: ?(Branch[]),
-  childNames: ?(string[])
+  children: Branch[],
+  childNames: string[],
+  parent: ?Branch,
+  siblings: ?(Branch[]),
+  root: boolean,
+  totalWeight: number
 };
+
+type BranchHash = { [string]: Branch };
 
 const PROGRAM_TEST = /^([a-z]+) \((\d+)\)(?: -> ((?:[a-z]+(?:, )?)+))?/;
 
@@ -12,30 +18,69 @@ const processRow = (row: string): Branch => {
   const [_, name, weightStr, childrenStr] = PROGRAM_TEST.exec(row);
   const weight = parseInt(weightStr, 10);
   const childNames = childrenStr && childrenStr.split(', ');
-  return { name, weight, children: null, childNames };
+
+  return { name, weight, children: [], parent: null, root: false, totalWeight: 0, childNames };
 };
 
-const sumBranch = (branch: Branch) => {
-  let total = branch.weight;
-  if (branch.children) {
-    total += branch.children.map(sumBranch).reduce((a, b) => a + b, 0);
-  }
-  return total;
-};
-
-const buildTree = (input: string[]): ?Branch => {
+const buildTree = (input: string[]): BranchHash => {
   const branches: Branch[] = input.map(processRow);
-  const allChildNames: string[] = [].concat(...branches.map(r => r.childNames).filter(Boolean));
   const hash: { [string]: Branch } = branches.reduce((obj, row) => {
     obj[row.name] = row;
     return obj;
   }, {});
 
-  branches.forEach(({ name, childNames }) => {
-    if (childNames) hash[name].children = childNames.map((childName: string) => hash[childName]);
+  branches.forEach(branch => {
+    const { name, childNames } = branch;
+
+    if (childNames) {
+      branch.children = childNames.map((childName: string) => {
+        const child = hash[childName];
+        child.parent = branch;
+        child.siblings = childNames.filter(name => name !== childName).map(name => hash[name]);
+
+        return child;
+      });
+    }
+
+    branch.totalWeight = sumBranch(branch);
   });
 
-  return branches.find(branch => !allChildNames.includes(branch.name));
+  const rootBranch = Object.keys(hash)
+    .map(key => hash[key])
+    .find(branch => !branch.parent);
+
+  if (rootBranch) rootBranch.root = true;
+
+  return hash;
 };
 
-module.exports = { buildTree, sumBranch };
+const sumBranch = (branch: Branch): number => {
+  let total = branch.weight;
+  total += branch.children.map(sumBranch).reduce((a, b) => a + b, 0);
+  return total;
+};
+
+type FindImbalancedBranchCallback = (branch: Branch, difference: number) => void;
+
+const allSameValue = (arr: number[]): boolean => new Set(arr).size === 1;
+
+const isProblemBranch = (branch: Branch): boolean => {
+  const hasChildren = !!branch.children && branch.children.length > 0;
+  const branchWeight = sumBranch(branch);
+
+  if (!branch.siblings) return false;
+
+  const siblingWeights = branch.siblings.map(sumBranch);
+  const outlier = allSameValue(siblingWeights) && branchWeight !== siblingWeights[0];
+
+  if (!outlier) return false;
+  if (hasChildren) return allSameValue(branch.children.map(sumBranch));
+  return true;
+};
+
+const findImbalancedBranch = (hash: BranchHash): ?Branch =>
+  Object.keys(hash)
+    .map(key => hash[key])
+    .find(isProblemBranch);
+
+module.exports = { buildTree, sumBranch, findImbalancedBranch };
--- 2017/day7.test.js
+++ 2017/day7.test.js
@@ -1,17 +1,40 @@
-const { sumBranch, buildTree } = require('./day7');
+const { sumBranch, buildTree, findImbalancedBranch } = require('./day7');
 const { EXAMPLE_INPUT, CHALLENGE_INPUT } = require('./day7.input');
 
 test('build tree for example input', () => {
-  const root = buildTree(EXAMPLE_INPUT);
+  const tree = buildTree(EXAMPLE_INPUT);
+  const root = Object.values(tree).find(b => b.root);
   expect(root.name).toEqual('tknk');
 });
 
 test('build tree for challenge input', () => {
-  const root = buildTree(CHALLENGE_INPUT);
+  const tree = buildTree(CHALLENGE_INPUT);
+  const root = Object.values(tree).find(b => b.root);
   expect(root.name).toEqual('vvsvez');
 });
 
 test('sum branch for example input', () => {
-  const root = buildTree(EXAMPLE_INPUT);
+  const tree = buildTree(EXAMPLE_INPUT);
+  const root = Object.values(tree).find(b => b.root);
   expect(sumBranch(root)).toEqual(778);
+});
+
+test('sum branch for challenge input', () => {
+  const tree = buildTree(CHALLENGE_INPUT);
+  const root = Object.values(tree).find(b => b.root);
+  expect(sumBranch(root)).toEqual(402167);
+});
+
+test('find imbalanced branch for example input', () => {
+  const tree = buildTree(EXAMPLE_INPUT);
+  const branch = findImbalancedBranch(tree);
+  const difference = sumBranch(branch.siblings[0]) - sumBranch(branch);
+  expect(branch.weight + difference).toEqual(60);
+});
+
+test('find imbalanced branch for challenge input', () => {
+  const tree = buildTree(CHALLENGE_INPUT);
+  const branch = findImbalancedBranch(tree);
+  const difference = sumBranch(branch.siblings[0]) - sumBranch(branch);
+  expect(branch.weight + difference).toEqual(60);
 });
