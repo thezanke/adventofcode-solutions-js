671ede45d88c897ec99f11c60d475dfba84d8769
Alex Howard <thezanke@gmail.com>
2017-12-23 02:23:04 -0500

    Finish day9

--- 2017/day9.js
+++ 2017/day9.js
@@ -1,28 +1,83 @@
 const initialState = {
-  groups: [],
-  group: null,
-  ignore: false,
+  nodes: {},
+  stack: [],
+  currentNode: null,
+  ignoring: false
 };
 
-const readCharacter = (state, char) => {
-  if (state.ignore) return { ...state, ignore: false };
-  if (char === '!') return { ...state, ignore: true };
+const createGarbage = (id, parentNode) => ({
+  id,
+  parentNode,
+  type: 'garbage',
+  contents: ''
+});
 
-  switch (char) {
-    case '{':
-      if 
-      break;
-    case '}':
-      break;
-    case '<':
-      break;
-    case '>':
-      break;
-    default:
+const createGroup = (id, parentNode) => ({
+  id,
+  parentNode,
+  type: 'group',
+  children: [],
+  score: parentNode ? parentNode.score + 1 : 1
+});
+
+const createNode = (() => {
+  let nextId = 1;
+
+  return (type, parentNode = null) => {
+    let node;
+
+    if (type === 'garbage') {
+      node = createGarbage(nextId++, parentNode);
+    } else if (type === 'group') {
+      node = createGroup(nextId++, parentNode);
+    } else {
+      throw Error('invalid node type');
+    }
+
+    if (parentNode && parentNode.children) parentNode.children.push(node);
+
+    return node;
+  };
+})();
+
+const readCharacter = (state, char, i) => {
+  if (state.ignoring) return { ...state, ignoring: false };
+  if (char === '!') return { ...state, ignoring: true };
+
+  const currentNode = state.stack.length ? state.stack[0] : null;
+
+  if (currentNode) {
+    if (currentNode.type === 'garbage' && char !== '>') {
+      currentNode.contents += char;
+      return state;
+    }
+    if (['}', '>'].includes(char)) return { ...state, stack: state.stack.slice(1) };
+    if (char === ',') return state;
   }
+
+  if (['<', '{'].includes(char)) {
+    const newNode = createNode(char === '<' ? 'garbage' : 'group', currentNode);
+    if (newNode.parentNode && newNode.parentNode.type === 'garbage') {
+      console.log({ char, i, newNode });
+      throw Error('impossible');
+    }
+    const nodes = { ...state.nodes, [newNode.id]: newNode };
+    const stack = [newNode, ...state.stack];
+
+    return { ...state, nodes, stack };
+  }
+
+  return state;
 };
 
-const processStream = (stream, groups = []) =>
-  stream.split('').reduce(readCharacter, initialState);
+const processStream = (stream, groups = []) => stream.split('').reduce(readCharacter, initialState);
 
-module.exports = { processStream };
+const countGroups = ({ nodes }) => Object.values(nodes).filter(n => n.type === 'group').length;
+
+const calculateTotal = ({ nodes }) =>
+  Object.values(nodes)
+    .filter(n => n.type === 'group')
+    .map(n => n.score)
+    .reduce((a, b) => a + b, 0);
+
+module.exports = { processStream, calculateTotal, countGroups };
--- 2017/day9.test.js
+++ 2017/day9.test.js
@@ -1,9 +1,28 @@
-const { processStream } = require('./day9');
+const { processStream, calculateTotal, countGroups } = require('./day9');
+const { CHALLENGE_INPUT } = require('./day9.input');
 
 test('process example input streams', () => {
-  expect(processStream('{}').length).toEqual(1);
-  expect(processStream('{{{}}}').length).toEqual(3);
-  // expect(processStream('{{},{}}').length).toEqual(3);
-  // expect(processStream('{{{},{},{{}}}}').length).toEqual(3);
-  // expect(processStream('{<{},{},{{}}>}').length).toEqual(1);
+  expect(countGroups(processStream('{}'))).toEqual(1);
+  expect(countGroups(processStream('{{{}}}'))).toEqual(3);
+  expect(countGroups(processStream('{{},{}}'))).toEqual(3);
+  expect(countGroups(processStream('{{{},{},{{}}}}'))).toEqual(6);
+  expect(countGroups(processStream('{<{},{},{{}}>}'))).toEqual(1);
+  expect(countGroups(processStream('{<a>,<a>,<a>,<a>}'))).toEqual(1);
+  expect(countGroups(processStream('{{<a>},{<a>},{<a>},{<a>}}'))).toEqual(5);
+  expect(countGroups(processStream('{{<!>},{<!>},{<!>},{<a>}}'))).toEqual(2);
 });
+
+test('calculate total scores for examples', () => {
+  expect(calculateTotal(processStream('{}'))).toEqual(1);
+  expect(calculateTotal(processStream('{{{}}}'))).toEqual(6);
+  expect(calculateTotal(processStream('{{},{}}'))).toEqual(5);
+  expect(calculateTotal(processStream('{{{},{},{{}}}}'))).toEqual(16);
+  expect(calculateTotal(processStream('{<a>,<a>,<a>,<a>}'))).toEqual(1);
+  expect(calculateTotal(processStream('{{<ab>},{<ab>},{<ab>},{<ab>}}'))).toEqual(9);
+  expect(calculateTotal(processStream('{{<!!>},{<!!>},{<!!>},{<!!>}}'))).toEqual(9);
+  expect(calculateTotal(processStream('{{<a!>},{<a!>},{<a!>},{<ab>}}'))).toEqual(3);
+});
+
+test('calculate total score for challenge', () => {
+  expect(calculateTotal(processStream(CHALLENGE_INPUT))).toEqual(12897);
+})
