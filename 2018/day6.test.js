6e76f8589d8c150ecc2b9a64f20c179d5e23b539
Alex Howard <me@alexhoward.io>
2018-12-06 00:09:54 -0500

    starting day 6 2018

---
+++ 2018/day6.test.js
@@ -0,0 +1,21 @@
+const { flatten } = require('lodash');
+
+const INPUT = require('./input/day6');
+
+const part1 = input => {
+  // return performReactions(input).length;
+};
+
+const part2 = input => {
+  // return Math.min(...mutations.map(p => p.length));
+};
+
+describe('day 6', () => {
+  test.skip('part 1', () => {
+    expect(part1(INPUT)).toEqual();
+  });
+
+  test.skip('part 2', () => {
+    expect(part2(INPUT)).toEqual();
+  });
+})
