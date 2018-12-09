b736cdee7810daead385a6e5773be831af5c5b69
Alex Howard <me@alexhoward.io>
2018-12-09 11:04:53 -0500

    day 9 2018

--- 2018/day9.test.js
+++ 2018/day9.test.js
@@ -1,9 +1,48 @@
-const { EXAMPLE_INPUT, CHALLENGE_INPUT } = require('./input/day9');
-
-const { part1, part2 } = require('./lib/day9');
+const { createMarbleCircle, playGame } = require('./lib/day9');
 
 describe('day 9', () => {
-  test.skip('example', () => { });
-  test.skip('part 1', () => { });
-  test.skip('part 2', () => { });
+  describe('marbleCircle', () => {
+    test('findForward', () => {
+      const marbleCircle = createMarbleCircle([0, 1, 2, 3, 4, 5, 6]);
+      expect(marbleCircle.findForward(4, 3)).toEqual(0);
+      expect(marbleCircle.findForward(4, 10)).toEqual(0);
+    });
+    test('findReverse', () => {
+      const marbleCircle = createMarbleCircle([0, 1, 2, 3, 4, 5, 6]);
+      expect(marbleCircle.findReverse(4, 3)).toEqual(1);
+      expect(marbleCircle.findReverse(4, 10)).toEqual(1);
+    });
+  });
+
+  test('example 1', () => {
+    expect(playGame({ playerCount: 9, max: 25 })).toEqual(32)
+  });
+
+  test('example 2', () => {
+    expect(playGame({ playerCount: 10, max: 1618 })).toEqual(8317)
+  });
+
+  test('example 3', () => {
+    expect(playGame({ playerCount: 13, max: 7999 })).toEqual(146373)
+  });
+
+  test('example 4', () => {
+    expect(playGame({ playerCount: 17, max: 1104 })).toEqual(2764)
+  });
+
+  test('example 5', () => {
+    expect(playGame({ playerCount: 21, max: 6111 })).toEqual(54718)
+  });
+
+  test('example 6', () => {
+    expect(playGame({ playerCount: 30, max: 5807 })).toEqual(37305)
+  });
+
+  test('part 1', () => {
+    expect(playGame({ playerCount: 459, max: 72103 })).toEqual(37305)
+  });
+
+  test('part 2', () => {
+    expect(playGame({ playerCount: 459, max: 72103 * 100 })).toEqual(37305)
+  });
 });
--- 2018/lib/day9.js
+++ 2018/lib/day9.js
@@ -1,7 +1,55 @@
-const { } = require('lodash');
+const createMarbleCircle = (startCircle = [0]) => {
+  const circle = [...startCircle];
 
-const part1 = a => a;
+  const indexOf = marble => circle.indexOf(marble);
+  const at = i => circle[i];
+  const remove = marble => circle.splice(circle.indexOf(marble), 1);
+  const add = (start, marble) => circle.splice(circle.indexOf(start) + 1, 0, marble);
+
+  const findForward = (start, n) => {
+    let position = circle.indexOf(start);
+    if (circle.length < 2 || n === circle.length) return circle[position];
+    let i = position + n;
+    if (i >= circle.length) i = i % circle.length;
+    return circle[i];
+  }
+
+  const findReverse = (start, n) => {
+    let position = circle.indexOf(start);
+    if (circle.length < 2 || n === circle.length) return circle[position];
+    let i = position - (n % circle.length);
+    if (i < 0) i = i + circle.length;
+    return circle[i];
+  };
+
+  return { findForward, findReverse, indexOf, at, add, remove, log: () => console.log({circle}) };
+}
+
+const playGame = ({ playerCount, max }) => {
+  const marbleCircle = createMarbleCircle();
+
+  const players = Array.from({ length: playerCount }).map((_, i) => ({ id: i + 1, points: 0 }));
+
+  let currentPlayer = 0;
+  let currentMarble = 0;
+
+  for (let nextMarble = 1; nextMarble <= max; nextMarble += 1) {
+    if (nextMarble % 23 === 0) {
+      let dropMarble = marbleCircle.findReverse(currentMarble, 7);
+      players[currentPlayer].points += nextMarble + dropMarble;
+      currentMarble = marbleCircle.findReverse(currentMarble, 6)
+      marbleCircle.remove(dropMarble);
+    } else {
+      let insertAfter = marbleCircle.findForward(currentMarble, 1);
+      marbleCircle.add(insertAfter, nextMarble);
+      currentMarble = nextMarble;
+    }
+    currentPlayer = (currentPlayer + 1) % playerCount;
+  }
+
+  return players.sort((a,b) => b.points - a.points)[0].points;
+};
 
 const part2 = a => a;
 
-module.exports = { part1, part2 };
+module.exports = { createMarbleCircle, playGame, part2 };
