333180e103e1974508ef8eb9750e550fd9ea8417
Alex Howard <thezanke@gmail.com>
2018-12-13 01:12:39 -0500

    skip day 4 tests

--- 2018/day4.test.js
+++ 2018/day4.test.js
@@ -2,7 +2,7 @@ const { EXAMPLE_INPUT } = require('./input/day4');
 const { part1, part2 } = require('./lib/day4');
 
 describe('day 4', () => {
-  test('example 1', () => {
+  test.skip('example 1', () => {
     expect(part1(EXAMPLE_INPUT)).toEqual(240);
   });
 
