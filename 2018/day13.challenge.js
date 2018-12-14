30cf5f99e03c3b4490f9b34ca0cda8fe9834946d
Alex Howard <thezanke@gmail.com>
2018-12-13 22:04:07 -0500

    remove challenge only script

--- 2018/day13.challenge.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-const { readFile } = require('./lib/utils');
-const { part1, part2 } = require('./lib/day13');
-
-const inputDir = path.join(__dirname, 'input', 'day13');
-
-readFile(path.join(inputDir, 'challenge.txt')).then(input => console.log(part2(input)));
