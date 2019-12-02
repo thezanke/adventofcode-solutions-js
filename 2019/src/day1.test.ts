d29bbbd7874d69a2acb74294b4161460bc5bb85c
Alex Howard <thezanke@gmail.com>
2019-12-02 13:09:52 -0500

    finished day 1

--- 2019/src/day1.test.ts
+++ 2019/src/day1.test.ts
@@ -1,5 +1,35 @@
-import { helloWorld } from './day1';
+import {
+  calculateAdditionalFuel,
+  calculateRequiredFuel,
+  day1part1,
+  day1part2,
+  getInput,
+} from './day1';
 
-test('helloWorld', () => {
-  expect(helloWorld()).toBeUndefined()
+test('getInput', async () => {
+  const input = await getInput();
+  expect(input).toBeInstanceOf(Array);
+  expect(input.length).toEqual(100);
+});
+
+test('calculateRequiredFuel', () => {
+  expect(calculateRequiredFuel(12)).toEqual(2);
+  expect(calculateRequiredFuel(14)).toEqual(2);
+  expect(calculateRequiredFuel(1969)).toEqual(654);
+  expect(calculateRequiredFuel(100756)).toEqual(33583);
+});
+
+test('day1part1', async () => {
+  const res = await day1part1();
+  expect(res).toEqual(3210097);
+});
+
+test('calculateAdditionalFuel', async () => {
+  expect(calculateAdditionalFuel(calculateRequiredFuel(1969))).toEqual(312);
+  expect(calculateAdditionalFuel(calculateRequiredFuel(100756))).toEqual(16763);
+});
+
+test('day1part2', async () => {
+  const res = await day1part2();
+  expect(res).toEqual(4812287);
 });
--- 2019/src/day1.ts
+++ 2019/src/day1.ts
@@ -1,1 +1,40 @@
-export const helloWorld = () => console.log('Hello World');
+import * as path from 'path';
+import { readFile } from 'fs-extra';
+
+const INPUT_PATH = path.resolve(__dirname, 'input', 'day1.txt');
+
+export const getInput = async () => {
+  const input = await readFile(INPUT_PATH, 'utf8');
+  return input.split('\n');
+};
+
+export const calculateRequiredFuel = (mass: number) => {
+  return Math.floor(mass / 3) - 2;
+};
+
+export const day1part1 = async () => {
+  const input = await getInput();
+  return input.reduce((total: number, mass: string) => {
+    return total + calculateRequiredFuel(Number(mass));
+  }, 0);
+};
+
+export const calculateAdditionalFuel = (initialFuel: number) => {
+  let nextRequiredAmount = calculateRequiredFuel(initialFuel);
+  let totalAdditional = nextRequiredAmount;
+
+  while (nextRequiredAmount > 0) {
+    nextRequiredAmount = calculateRequiredFuel(nextRequiredAmount);
+    if (nextRequiredAmount > 0) totalAdditional += nextRequiredAmount;
+  }
+
+  return totalAdditional;
+};
+
+export const day1part2 = async () => {
+  const input = await getInput();
+  return input.reduce((total: number, mass: string) => {
+    const initialFuel = calculateRequiredFuel(Number(mass));
+    return total + initialFuel + calculateAdditionalFuel(initialFuel);
+  }, 0);
+};
