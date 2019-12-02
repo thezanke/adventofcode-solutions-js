3a18c0316597e6485cad1acd26087b7abf33a646
Alex Howard <thezanke@gmail.com>
2019-12-02 13:15:20 -0500

    clean up test names

--- 2019/src/day1.test.ts
+++ 2019/src/day1.test.ts
@@ -6,30 +6,30 @@ import {
   getInput,
 } from './day1';
 
-test('getInput', async () => {
+test(`DAY 1: getInput()`, async () => {
   const input = await getInput();
   expect(input).toBeInstanceOf(Array);
   expect(input.length).toEqual(100);
 });
 
-test('calculateRequiredFuel', () => {
+test('DAY 1: calculateRequiredFuel()', () => {
   expect(calculateRequiredFuel(12)).toEqual(2);
   expect(calculateRequiredFuel(14)).toEqual(2);
   expect(calculateRequiredFuel(1969)).toEqual(654);
   expect(calculateRequiredFuel(100756)).toEqual(33583);
 });
 
-test('day1part1', async () => {
+test('DAY 1: Part 1', async () => {
   const res = await day1part1();
   expect(res).toEqual(3210097);
 });
 
-test('calculateAdditionalFuel', async () => {
+test('DAY 1: calculateAdditionalFuel()', async () => {
   expect(calculateAdditionalFuel(calculateRequiredFuel(1969))).toEqual(312);
   expect(calculateAdditionalFuel(calculateRequiredFuel(100756))).toEqual(16763);
 });
 
-test('day1part2', async () => {
+test('DAY 1: Part 2', async () => {
   const res = await day1part2();
   expect(res).toEqual(4812287);
 });
