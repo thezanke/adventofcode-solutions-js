8cbd0390c9427fe3c0a3718e7aecfd88a41ae641
Alex Howard <thezanke@gmail.com>
2019-12-03 02:24:27 -0500

    adding describe blocks

--- 2019/src/day1.test.ts
+++ 2019/src/day1.test.ts
@@ -5,24 +5,28 @@ import {
   solvePart2,
 } from './day1';
 
-test('DAY 1: calculateRequiredFuel()', () => {
-  expect(calculateRequiredFuel(12)).toEqual(2);
-  expect(calculateRequiredFuel(14)).toEqual(2);
-  expect(calculateRequiredFuel(1969)).toEqual(654);
-  expect(calculateRequiredFuel(100756)).toEqual(33583);
-});
+describe('DAY 1', () => {
+  test('calculateRequiredFuel()', () => {
+    expect(calculateRequiredFuel(12)).toEqual(2);
+    expect(calculateRequiredFuel(14)).toEqual(2);
+    expect(calculateRequiredFuel(1969)).toEqual(654);
+    expect(calculateRequiredFuel(100756)).toEqual(33583);
+  });
 
-test('DAY 1: Part 1', async () => {
-  const res = await solvePart1();
-  expect(res).toEqual(3210097);
-});
+  test('Part 1', async () => {
+    const res = await solvePart1();
+    expect(res).toEqual(3210097);
+  });
 
-test('DAY 1: calculateAdditionalFuel()', async () => {
-  expect(calculateAdditionalFuel(calculateRequiredFuel(1969))).toEqual(312);
-  expect(calculateAdditionalFuel(calculateRequiredFuel(100756))).toEqual(16763);
-});
+  test('calculateAdditionalFuel()', async () => {
+    expect(calculateAdditionalFuel(calculateRequiredFuel(1969))).toEqual(312);
+    expect(calculateAdditionalFuel(calculateRequiredFuel(100756))).toEqual(
+      16763
+    );
+  });
 
-test('DAY 1: Part 2', async () => {
-  const res = await solvePart2();
-  expect(res).toEqual(4812287);
+  test('Part 2', async () => {
+    const res = await solvePart2();
+    expect(res).toEqual(4812287);
+  });
 });
--- 2019/src/day2.test.ts
+++ 2019/src/day2.test.ts
@@ -1,21 +1,23 @@
 import { runProgram, solvePart1, solvePart2 } from './day2';
 
-test('Day 2: runProgram()', () => {
-  expect(runProgram([1, 0, 0, 0, 99])).toEqual([2, 0, 0, 0, 99]);
-  expect(runProgram([2, 0, 0, 3, 99], [3, 0])).toEqual([2, 3, 0, 6, 99]);
-  expect(runProgram([2, 4, 4, 5, 99, 0])).toEqual([2, 4, 4, 5, 99, 9801]);
+describe('DAY 2', () => {
+  test('runProgram()', () => {
+    expect(runProgram([1, 0, 0, 0, 99])).toEqual([2, 0, 0, 0, 99]);
+    expect(runProgram([2, 0, 0, 3, 99], [3, 0])).toEqual([2, 3, 0, 6, 99]);
+    expect(runProgram([2, 4, 4, 5, 99, 0])).toEqual([2, 4, 4, 5, 99, 9801]);
 
-  const input = [1, 1, 1, 4, 99, 5, 6, 0, 99];
-  const output = [30, 1, 1, 4, 2, 5, 6, 0, 99];
-  expect(runProgram(input)).toEqual(output);
-});
+    const input = [1, 1, 1, 4, 99, 5, 6, 0, 99];
+    const output = [30, 1, 1, 4, 2, 5, 6, 0, 99];
+    expect(runProgram(input)).toEqual(output);
+  });
 
-test('Day 2: Part 1', async () => {
-  const res = await solvePart1();
-  expect(res).toBe(6730673);
-});
+  test('Part 1', async () => {
+    const res = await solvePart1();
+    expect(res).toBe(6730673);
+  });
 
-test('Day 2: Part 2', async () => {
-  const res = await solvePart2(19690720);
-  expect(res).toBe(3749);
+  test('Part 2', async () => {
+    const res = await solvePart2(19690720);
+    expect(res).toBe(3749);
+  });
 });
