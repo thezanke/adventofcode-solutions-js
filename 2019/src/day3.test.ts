81136cfe29b6f2d588b394b5af59ae92038c0eee
Alex Howard <thezanke@gmail.com>
2019-12-03 03:12:50 -0500

    its not pretty, but it works

--- 2019/src/day3.test.ts
+++ 2019/src/day3.test.ts
@@ -1,5 +1,163 @@
-import { solvePart1 } from './day3';
+import {
+  solvePart1,
+  getNextSegment,
+  getWirePoints,
+  findIntersections,
+  determineClosestIntersection,
+  solvePart2,
+} from './day3';
 
-test('Day 3: Part 1', () => {
-  expect(solvePart1()).toBeUndefined();
+import { getInput } from './utils/getInput';
+
+describe('Day 3', () => {
+  test('getNextSegment()', () => {
+    expect(getNextSegment({ x: 0, y: 0 }, 'U3')).toEqual([
+      { x: 0, y: -1 },
+      { x: 0, y: -2 },
+      { x: 0, y: -3 },
+    ]);
+
+    expect(getNextSegment({ x: 0, y: -3 }, 'R2')).toEqual([
+      { x: 1, y: -3 },
+      { x: 2, y: -3 },
+    ]);
+  });
+
+  test('getWirePoints()', () => {
+    expect(getWirePoints(['U3'])).toEqual([
+      { x: 0, y: -1 },
+      { x: 0, y: -2 },
+      { x: 0, y: -3 },
+    ]);
+
+    expect(getWirePoints(['U3', 'D1', 'L2'])).toEqual([
+      { x: 0, y: -1 },
+      { x: 0, y: -2 },
+      { x: 0, y: -3 },
+      { x: 0, y: -2 },
+      { x: -1, y: -2 },
+      { x: -2, y: -2 },
+    ]);
+  });
+
+  test('findIntersections()', () => {
+    const wire1 = [
+      { x: 0, y: -1 },
+      { x: 0, y: -2 },
+      { x: 0, y: -3 },
+      { x: 0, y: -4 },
+      { x: 1, y: -4 },
+      { x: 2, y: -4 },
+      { x: 3, y: -4 },
+    ];
+
+    const wire2 = [
+      { x: -1, y: -2 },
+      { x: 0, y: -2 },
+      { x: 1, y: -2 },
+      { x: 2, y: -2 },
+      { x: 3, y: -2 },
+      { x: 3, y: -3 },
+      { x: 3, y: -4 },
+    ];
+
+    const intersections = findIntersections(wire1, wire2);
+    expect(intersections).toEqual([
+      { x: 0, y: -2, totalSteps: 4 },
+      { x: 3, y: -4, totalSteps: 14 },
+    ]);
+  });
+
+  test('determineClosestIntersection()', () => {
+    expect(
+      determineClosestIntersection([
+        { x: 2, y: -5 },
+        { x: 0, y: -10 },
+      ])
+    ).toEqual(7);
+  });
+
+  test('Part 1 - Examples', async () => {
+    expect(
+      solvePart1([
+        ['R8', 'U5', 'L5', 'D3'],
+        ['U7', 'R6', 'D4', 'L4'],
+      ])
+    ).toEqual(6);
+
+    expect(
+      solvePart1([
+        ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
+        ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'],
+      ])
+    ).toEqual(159);
+
+    expect(
+      solvePart1([
+        [
+          'R98',
+          'U47',
+          'R26',
+          'D63',
+          'R33',
+          'U87',
+          'L62',
+          'D20',
+          'R33',
+          'U53',
+          'R51',
+        ],
+        ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7'],
+      ])
+    ).toEqual(135);
+  });
+
+  test.skip('Part 1 - Final Input', async () => {
+    const finalInput = (await getInput('day3.txt', '\n', line =>
+      line.split(',')
+    )) as [string[], string[]];
+    expect(solvePart1(finalInput)).toEqual(100);
+  });
+
+  test('Part 2 - Examples', () => {
+    expect(
+      solvePart2([
+        ['R8', 'U5', 'L5', 'D3'],
+        ['U7', 'R6', 'D4', 'L4'],
+      ])
+    ).toEqual(30);
+
+    expect(
+      solvePart2([
+        ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
+        ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'],
+      ])
+    ).toEqual(610);
+
+    expect(
+      solvePart2([
+        [
+          'R98',
+          'U47',
+          'R26',
+          'D63',
+          'R33',
+          'U87',
+          'L62',
+          'D20',
+          'R33',
+          'U53',
+          'R51',
+        ],
+        ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7'],
+      ])
+    ).toEqual(410);
+  });
+
+  test.skip('Part 2 - Final Input', async () => {
+    const finalInput = (await getInput('day3.txt', '\n', line =>
+      line.split(',')
+    )) as [string[], string[]];
+    expect(solvePart2(finalInput)).toEqual(100);
+  });
 });
--- 2019/src/day3.ts
+++ 2019/src/day3.ts
@@ -1,3 +1,95 @@
-export const solvePart1 = () => {
-  return;
+import * as _ from 'lodash';
+
+interface Point {
+  x: number;
+  y: number;
 }
+
+interface Intersection extends Point {
+  totalSteps: number;
+}
+
+enum DIRECTION {
+  UP = 'U',
+  DOWN = 'D',
+  LEFT = 'L',
+  RIGHT = 'R',
+}
+
+export const getNextSegment = (lastPoint: Point, nextMove: string) => {
+  const prefix = nextMove[0];
+  const distance = Number(nextMove.slice(1));
+  const segment: Point[] = [];
+
+  const { x, y } = lastPoint;
+
+  for (let i = 1; i <= distance; i += 1) {
+    switch (prefix) {
+      case DIRECTION.UP:
+        segment.push({ x, y: y - i });
+        break;
+      case DIRECTION.DOWN:
+        segment.push({ x, y: y + i });
+        break;
+      case DIRECTION.LEFT:
+        segment.push({ x: x - i, y });
+        break;
+      case DIRECTION.RIGHT:
+        segment.push({ x: x + i, y });
+        break;
+      default:
+        throw Error('Not a valid direction ' + prefix);
+    }
+  }
+
+  return segment;
+};
+
+export const getWirePoints = (route: string[]) => {
+  return route.reduce<Point[]>((points, nextMove) => {
+    let lastPoint = points[points.length - 1];
+    if (!lastPoint) lastPoint = { x: 0, y: 0 };
+    points.push(...getNextSegment(lastPoint, nextMove));
+    return points;
+  }, []);
+};
+
+export const findIntersections = (wire1: Point[], wire2: Point[]) => {
+  const intersections: Intersection[] = [];
+  wire1.forEach(({ x, y }, i) => {
+    const xPointIndex = wire2.findIndex(p => p.x === x && p.y === y);
+    const xPoint = wire2[xPointIndex];
+
+    if (xPoint) {
+      intersections.push({ ...xPoint, totalSteps: i + xPointIndex + 2 });
+    }
+  });
+  return intersections;
+};
+
+export const determineClosestIntersection = (intersections: Point[]) => {
+  const distances = intersections.map(
+    p => Math.abs(0 - p.x) + Math.abs(0 - p.y)
+  );
+
+  return Math.min(...distances);
+};
+
+export const solvePart1 = (input: [string[], string[]]) => {
+  const [wire1, wire2] = input.map(getWirePoints);
+  const intersections = findIntersections(wire1, wire2);
+  return determineClosestIntersection(intersections);
+};
+
+export const determineShortestIntersection = (
+  intersections: Intersection[]
+) => {
+  const stepsArr = _.map(intersections, 'totalSteps')
+  return Math.min(...stepsArr);
+};
+
+export const solvePart2 = (input: [string[], string[]]) => {
+  const [wire1, wire2] = input.map(getWirePoints);
+  const intersections = findIntersections(wire1, wire2);
+  return determineShortestIntersection(intersections);
+};
