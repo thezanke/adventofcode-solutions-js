7df53be8879b758545444b43e5b95ab8b7f92012
Alex Howard <thezanke@gmail.com>
2019-12-05 03:26:12 -0500

    day 5 complete

--- 2019/src/day5.test.ts
+++ 2019/src/day5.test.ts
@@ -1,5 +1,5 @@
 import { getInput } from './utils/getInput';
-import { solvePart1 } from './day5';
+import { solvePart1, solvePart2 } from './day5';
 
 describe('DAY 5', () => {
   const challengeInputPromise = getInput('day5.txt', ',', n => Number(n));
@@ -11,10 +11,10 @@ describe('DAY 5', () => {
     expect(mockFn).toHaveBeenLastCalledWith(16348437);
   });
 
-  // test('Part 2', async () => {
-  //   const challengeInput = await challengeInputPromise;
-  //   const mockFn = jest.fn();
-  //   solvePart1(challengeInput, mockFn);
-  //   expect(mockFn).toHaveBeenLastCalledWith(16348437);
-  // });
+  test('Part 2', async () => {
+    const challengeInput = await challengeInputPromise;
+    const mockFn = jest.fn();
+    solvePart2(challengeInput, mockFn);
+    expect(mockFn).toHaveBeenLastCalledWith(6959377);
+  });
 });
--- 2019/src/day5.ts
+++ 2019/src/day5.ts
@@ -6,3 +6,9 @@ export const solvePart1 = async (
 ) => {
   return runProgram(challengeInput, undefined, [1], outputHandler);
 };
+export const solvePart2 = async (
+  challengeInput: number[],
+  outputHandler: Function
+) => {
+  return runProgram(challengeInput, undefined, [5], outputHandler);
+};
