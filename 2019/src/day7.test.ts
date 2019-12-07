b4eea3fe51a723cc27de8914d4790e6bd0378951
Alex Howard <thezanke@gmail.com>
2019-12-07 16:56:03 -0500

    improving tests

--- 2019/src/day7.test.ts
+++ 2019/src/day7.test.ts
@@ -5,31 +5,29 @@ describe('DAY 7', () => {
   const challengeInputPromise = getInput('day7.txt', ',', n => Number(n));
 
   describe('Amplifier', () => {
-    test('produces an output', () => {
-      const amp = new Amplifier(
-        [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0],
-        1
-      );
-
+    test('waits for input, produces output, exits', () => {
+      // prettier-ignore
+      const initialMemory = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0];
+      const amp = new Amplifier(initialMemory, 1);
+      expect(amp.outputSignal).toBeNaN();
+      expect(amp.exited).toBeFalsy();
       amp.inputSignal = 1;
-
       expect(amp.outputSignal).toEqual(11);
+      expect(amp.exited).toBeTruthy();
     });
   });
 
   describe('runAmplifiers()', () => {
     test('solves linear problem', () => {
       // prettier-ignore
-      const input = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0];
-      expect(runAmplifiers(input, [4, 3, 2, 1, 0])).toEqual(43210);
+      const initalMemory = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0];
+      expect(runAmplifiers(initalMemory, [4, 3, 2, 1, 0])).toEqual(43210);
     });
 
     test('solves loopback problem', () => {
       // prettier-ignore
       const input = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5];
-      expect(runAmplifiers(input, [9, 8, 7, 6, 5], true)).toEqual(
-        139629729
-      );
+      expect(runAmplifiers(input, [9, 8, 7, 6, 5], true)).toEqual(139629729);
     });
   });
 
