bfd900b005920bc6ec55467c6d75e81e88bf9a72
Alex Howard <thezanke@gmail.com>
2019-12-07 16:29:09 -0500

    cleaning up permute function

--- 2019/src/utils/permute.ts
+++ 2019/src/utils/permute.ts
@@ -1,26 +1,27 @@
+// tslint:disable no-any
 /**
  * Credit to https://xaviergeerinck.com/solve-permutation-heaps
  * all I did was add types and a calling function with a result array.
  */
 export const permute = (input: any[]) => {
-  let result: any[][] = [];
+  const result: any[][] = [];
 
   function swap(arr: any[], idxA: number, idxB: number) {
-    var tmp = arr[idxA];
+    const tmp = arr[idxA];
     arr[idxA] = arr[idxB];
     arr[idxB] = tmp;
   }
 
   function generate(n: number, arr: any[]) {
-    if (n == 1) {
+    if (n === 1) {
       result.push(arr.slice());
       return;
     }
 
-    for (var i = 0; i < n; i += 1) {
+    for (let i = 0; i < n; i += 1) {
       generate(n - 1, arr);
 
-      if (n % 2 == 0) {
+      if (n % 2 === 0) {
         swap(arr, i, n - 1);
       } else {
         swap(arr, 0, n - 1);
