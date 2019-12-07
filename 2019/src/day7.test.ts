39d083481423e4a9ddf2f28d6427d92e691735ea
Alex Howard <thezanke@gmail.com>
2019-12-07 02:14:25 -0500

    part 1 complete

--- 2019/src/day7.test.ts
+++ 2019/src/day7.test.ts
@@ -1,17 +1,30 @@
-import { solvePart1, applyAmplifiers } from './day7';
+import { solvePart1, runAmplifiers, getPermutations } from './day7';
 import { getInput } from './utils/getInput';
 describe('DAY 7', () => {
-//   const challengeInputPromise = getInput('day5.txt', ',', n => Number(n));
+  const challengeInputPromise = getInput('day7.txt', ',', n => Number(n));
 
-  test('applyAmplifiers()', () => {
-    const input = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]; // prettier-ignore
-    expect(applyAmplifiers(input, [4,3,2,1,0])).toEqual(43210);
+  test('createAmplifier()', () => {
+    // prettier-ignore
+    const input = [
+      3, 15, 3, 16, 1002, 16, 10, 16,
+      1, 16, 15, 15, 4, 15, 99, 0, 0,
+    ];
+
+    expect(runAmplifiers(input, [4, 3, 2, 1, 0])).toEqual(43210);
   });
 
-  // describe('Part 1', () => {
-  //     test('solves Example input', async () => {
-  //         const input = await challengeInputPromise as number[];
-  //         expect(solvePart1(input)).toEqual(true);
-  //     });
-  // })
+  test('getPermutations()', () => {
+    // prettier-ignore
+    expect(getPermutations([0, 1, 2])).toEqual([
+      [0, 1, 2], [0, 2, 1], [1, 0, 2],
+      [1, 2, 0], [2, 0, 1], [2, 1, 0],
+    ]);
+  });
+
+  describe('Part 1', () => {
+    test('solves example input', async () => {
+      const input = (await challengeInputPromise) as number[];
+      expect(solvePart1(input)).toEqual(273814);
+    });
+  });
 });
--- 2019/src/day7.ts
+++ 2019/src/day7.ts
@@ -3,12 +3,9 @@ import { runProgram } from './runProgram';
 // INPUT phaseSetting
 // INPUT inputSignal
 
-const DEBUG = true;
+const DEBUG = false;
 
-export const applyAmplifiers = (
-  intcode: number[],
-  phases: [number, number, number, number, number]
-) => {
+export const runAmplifiers = (intcode: number[], phases: number[]) => {
   const amplify = (phase: number, inputSignal: number) => {
     let outputSignal: number | undefined;
     runProgram(
@@ -23,14 +20,37 @@ export const applyAmplifiers = (
     return outputSignal;
   };
 
+  if (DEBUG) console.log('RUNNING AMPLIFIERS ', { phases });
+
   return phases.reduce((inputSignal: number, phase: number) => {
-    console.log('AMPLIFYING: INPUT', { inputSignal, phase });
+    if (DEBUG) console.log('AMPLIFYING: INPUT', { inputSignal, phase });
     const outputSignal = amplify(phase, inputSignal);
-    console.log('AMPLIFYING: OUTPUT', { outputSignal });
+    if (DEBUG) console.log('AMPLIFYING: OUTPUT', { outputSignal });
     return outputSignal || 0;
   }, 0);
 };
 
+export const getPermutations = (inputArr: any[]): number[] | number[][] => {
+  const results: number[] = [];
+
+  const permute = (arr: any[], memo: any = []) => {
+    let current;
+
+    for (var i = 0; i < arr.length; i++) {
+      current = arr.splice(i, 1);
+      if (arr.length === 0) results.push(memo.concat(current));
+      permute(arr.slice(), memo.concat(current));
+      arr.splice(i, 0, current[0]);
+    }
+
+    return results;
+  };
+
+  return permute(inputArr);
+};
+
 export const solvePart1 = (intcode: number[]) => {
-  let output: number;
+  const perms = getPermutations([0, 1, 2, 3, 4]) as number[][];
+  const results = perms.map(phases => runAmplifiers(intcode, phases));
+  return Math.max(...results);
 };
