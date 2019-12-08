a7513f4beb45ecc58f3643a11c60ab35bb253b08
Alex Howard <thezanke@gmail.com>
2019-12-08 01:21:27 -0500

    tidy

--- 2019/src/day8.test.ts
+++ 2019/src/day8.test.ts
@@ -6,11 +6,13 @@ describe('DAY 8', () => {
     const input = '123456789012'.split('');
     expect(solvePart1(input, 3, 2)).toEqual(1);
   });
+
   const inputPromise = getInput('day8.txt', '');
   test('Part 1', async () => {
     const input = await inputPromise;
     expect(solvePart1(input, 25, 6)).toEqual(1792);
   });
+
   test('Part 2', async () => {
     const input = await inputPromise;
     // prettier-ignore
