313c4977c048c966f88a0010ddc08a372cf566b8
Alex Howard <thezanke@gmail.com>
2019-12-08 01:20:19 -0500

    finished part 2

--- 2019/src/day8.test.ts
+++ 2019/src/day8.test.ts
@@ -1,13 +1,26 @@
-import { solvePart1 } from './day8';
+import { solvePart1, solvePart2 } from './day8';
 import { getInput } from './utils/getInput';
 
 describe('DAY 8', () => {
-    test('example', () => {
-        const input = '123456789012'.split('');
-        expect(solvePart1(input, 3, 2)).toEqual(1);
-    })
-    test('Part 1', async () => {
-        const input = await getInput('day8.txt', '');
-        expect(solvePart1(input, 25, 6)).toEqual(100);
-    })
-})
+  test('example', () => {
+    const input = '123456789012'.split('');
+    expect(solvePart1(input, 3, 2)).toEqual(1);
+  });
+  const inputPromise = getInput('day8.txt', '');
+  test('Part 1', async () => {
+    const input = await inputPromise;
+    expect(solvePart1(input, 25, 6)).toEqual(1792);
+  });
+  test('Part 2', async () => {
+    const input = await inputPromise;
+    // prettier-ignore
+    expect(solvePart2(input, 25, 6)).toEqual(
+      '#      ## ####  ##  #  # \n' +
+      '#       # #    #  # #  # \n' +
+      '#       # ###  #    #### \n' +
+      '#       # #    #    #  # \n' +
+      '#    #  # #    #  # #  # \n' +
+      '####  ##  ####  ##  #  # '
+    );
+  });
+});
--- 2019/src/day8.ts
+++ 2019/src/day8.ts
@@ -1,8 +1,15 @@
 import * as _ from 'lodash';
 
+enum Color {
+  black,
+  white,
+  transparent,
+}
+
 class Layer {
   zeroCount: number;
-  constructor(public rows: number[][]) {
+
+  constructor(public rows: Color[][]) {
     this.zeroCount = 0;
     rows.forEach(row => {
       this.zeroCount += row.filter(n => n === 0).length;
@@ -10,7 +17,7 @@ class Layer {
   }
 }
 
-export const getLayers = (input: number[], width: number, height: number) => {
+export const getLayers = (input: Color[], width: number, height: number) => {
   const rows = _.chunk(input, width);
   const layers = _.map(_.chunk(rows, height), rows => new Layer(rows));
   return layers;
@@ -23,4 +30,20 @@ export const solvePart1 = (input: string[], width: number, height: number) => {
   const ones = digits.filter(n => n === 1).length;
   const twos = digits.filter(n => n === 2).length;
   return ones * twos;
+};
+
+export const solvePart2 = (input: string[], width: number, height: number) => {
+  const layers = getLayers(input.map(Number), width, height);
+  const imageRows = Array.from({ length: height }, (_v, rowIndex) =>
+    Array.from({ length: width }, (_v, columnIndex) => {
+      return layers
+        .map(layer => layer.rows[rowIndex][columnIndex])
+        .find(n => n != Color.transparent);
+    })
+  );
+  const image = imageRows
+    .map(row => row.map(c => (c === Color.black ? ' ' : '#')).join(''))
+    .join('\n');
+
+  return image;
 };
