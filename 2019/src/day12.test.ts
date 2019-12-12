53e30f1c683bd6db8fdcf3d780c083d50b1d98c2
Alex Howard <thezanke@gmail.com>
2019-12-12 16:01:24 -0500

    part 2

--- 2019/src/day12.test.ts
+++ 2019/src/day12.test.ts
@@ -1,10 +1,16 @@
-import { solvePart1 } from './day12';
+import { solvePart1, solvePart2 } from './day12';
 
-const EXAMPLE_INPUT = [
+const EXAMPLE1_INPUT = [
   '<x=-1, y=0, z=2>',
   '<x=2, y=-10, z=-7>',
   '<x=4, y=-8, z=8>',
   '<x=3, y=5, z=-1>',
+];
+const EXAMPLE2_INPUT = [
+  '<x=-8, y=-10, z=0>',
+  '<x=5, y=5, z=10>',
+  '<x=2, y=-7, z=3>',
+  '<x=9, y=-8, z=-3>',
 ];
 
 const FINAL_INPUT = [
@@ -17,10 +23,25 @@ const FINAL_INPUT = [
 describe('DAY 12', () => {
   describe('Part 1', () => {
     test('Example', () => {
-      expect(solvePart1(EXAMPLE_INPUT, 10)).toEqual(179);
+      expect(solvePart1(EXAMPLE1_INPUT, 10)).toEqual(179);
     });
+
     test('Final', () => {
       expect(solvePart1(FINAL_INPUT, 1000)).toEqual(8362);
+    });
+  });
+
+  describe('Part 2', () => {
+    test('Example 1', () => {
+      expect(solvePart2(EXAMPLE1_INPUT, 2800)).toEqual(2772);
+    });
+
+    test('Example 2', () => {
+      expect(solvePart2(EXAMPLE2_INPUT, 5000000000)).toEqual(4686774924);
+    });
+
+    test('Final', () => {
+      expect(solvePart2(FINAL_INPUT)).toEqual(478373365921244);
     });
   });
 });
--- 2019/src/day12.ts
+++ 2019/src/day12.ts
@@ -4,23 +4,52 @@ interface Vector {
   z: number;
 }
 
+const { PerformanceObserver, performance } = require('perf_hooks');
+
+const obs = new PerformanceObserver((items: any) => {
+  console.log(items.getEntries()[0].duration);
+  performance.clearMarks();
+});
+obs.observe({ entryTypes: ['measure'] });
+
+type Axis = 'x' | 'y' | 'z';
+
+const DIMENSIONS: Axis[] = ['x', 'y', 'z'];
+
 class Moon {
   pos: Vector;
   vel: Vector = { x: 0, y: 0, z: 0 };
   pNrg = 0;
   kNrg = 0;
+
   constructor(_pos: Vector, _vel?: Vector) {
     this.pos = { ..._pos };
     if (_vel) this.vel = { ..._vel };
   }
+
   get energy() {
     return this.pNrg * this.kNrg;
+  }
+
+  get state() {
+    const { pos, vel } = this;
+    // return `${pos.x},${pos.y},${pos.z},${vel.x},${vel.y},${vel.z}`;
+    return (
+      pos.x * 1e10 +
+      pos.y * 1e8 +
+      pos.z * 1e6 +
+      vel.x * 1e4 +
+      vel.y * 1e2 +
+      vel.z
+    );
+  }
+
+  getAxisState(axis: Axis) {
+    return this.pos[axis] * 1000 + this.vel[axis];
   }
 }
 
 const MOON_COORDINATES = /<x=(-?\d+), y=(-?\d+), z=(-?\d+)>/;
-
-const DIMENSIONS: Array<'x' | 'y' | 'z'> = ['x', 'y', 'z'];
 
 const vectorEnergy = (v: Vector) => {
   return Math.abs(v.x) + Math.abs(v.y) + Math.abs(v.z);
@@ -28,7 +57,6 @@ const vectorEnergy = (v: Vector) => {
 
 export class Simulation {
   moons: Moon[];
-  step = 0;
 
   constructor(input: string[]) {
     this.moons = this.parseInput(input);
@@ -75,7 +103,6 @@ export class Simulation {
 
   next = () => {
     this.applyUpdates();
-    this.step += 1;
   };
 
   get totalEnergy() {
@@ -83,10 +110,61 @@ export class Simulation {
       return total + moon.energy;
     }, 0);
   }
+
+  get state() {
+    return (
+      this.moons[0].state * 1e6 +
+      this.moons[1].state * 1e4 +
+      this.moons[2].state * 1e2 +
+      this.moons[3].state
+    );
+  }
+
+  getAxisState(axis: Axis) {
+    return this.moons.map(m => m.getAxisState(axis)).join(',');
+  }
 }
 
 export const solvePart1 = (input: string[], length = 1) => {
   const sim = new Simulation(input);
-  Array.from({ length }).forEach(sim.next);
+  for (let i = 0; i < length; i += 1) {
+    sim.next();
+  }
   return sim.totalEnergy;
+};
+
+/**
+ * Shamelessly stolen from a discussion with Alex D.
+ */
+const lcm = (array: number[]) => {
+  const n = array.length;
+  let a = array[0];
+  for (let i = 1; i < n; i++) {
+    let b = array[i];
+    const c = a;
+    while (a && b) {
+      a > b ? (a %= b) : (b %= a);
+    }
+    a = (c * array[i]) / (a + b);
+  }
+  return a;
+};
+
+export const solvePart2 = (input: string[], maxIterations = 5e9) => {
+  const sim = new Simulation(input);
+  sim.next();
+
+  const results = { x: 0, y: 0, z: 0 };
+
+  DIMENSIONS.forEach(axis => {
+    const initial = sim.getAxisState(axis);
+
+    while (true) {
+      sim.next();
+      results[axis] += 1;
+      if (sim.getAxisState(axis) === initial) break;
+    }
+  });
+
+  return lcm(DIMENSIONS.map(a => results[a]));
 };
