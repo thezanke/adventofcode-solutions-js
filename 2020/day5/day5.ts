99fd264127b7d41ecedb82297347b63469202d04
Alex Howard <thezanke@gmail.com>
2020-12-04 23:56:58 -0500

    init day5

--- 2020/day5/day5.ts
+++ 2020/day5/day5.ts
@@ -1,20 +1,3 @@
-import { Vect } from "../common/Vect.ts";
-
-const TREE = "#";
-
-export const countTrees = (lines: string[], trajectory: Vect) => {
-  const lineLength = lines[0].length;
-  const position = new Vect();
-
-  let treeCount = 0;
-
-  while (position.y < lines.length) {
-    const { x, y } = position;
-    const linePos = x % lineLength;
-    const posChar = lines[y][linePos];
-    if (posChar === TREE) treeCount += 1;
-    position.add(trajectory);
-  }
-
-  return treeCount;
+export const countTrees = () => {
+  return true;
 };
