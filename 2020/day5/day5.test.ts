1e54064ea49e5f1a37bd2ddfac3a6238a530dfae
Alex Howard <thezanke@gmail.com>
2020-12-05 00:35:16 -0500

    part2!

--- 2020/day5/day5.test.ts
+++ 2020/day5/day5.test.ts
@@ -1,18 +1,19 @@
 import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
 import { readInputFile } from "../common/readInputFile.ts";
-import { countTrees } from "./day5.ts";
+import { findSeat, getSeatId } from "./day5.ts";
 
 const input = readInputFile("day5/input.txt");
-const exampleInput = [true];
 
 Deno.test("Day 5 - Part 1 - Example 1", () => {
-  assertEquals(countTrees(), true);
+  assertEquals(getSeatId("FBFBBFFRLR"), 357);
 });
 
 Deno.test("Day 5 - Part 1 - Answer", () => {
-  assertEquals(countTrees(), true);
+  const ids = input.map((r) => getSeatId(r));
+  assertEquals(Math.max(...ids), 896);
 });
 
 Deno.test("Day 5 - Part 2 - Answer", () => {
-  assertEquals(countTrees(), true);
+  const ids = input.map((r) => getSeatId(r));
+  assertEquals(findSeat(ids), 1);
 });
--- 2020/day5/day5.ts
+++ 2020/day5/day5.ts
@@ -1,3 +1,51 @@
-export const countTrees = () => {
-  return true;
+const MAX_ROWS = 128;
+
+const PASS_MATCHER = /(.{7})(.{3})/;
+
+export const determineRow = (part: string) => {
+  let min = 0;
+  let max = 127;
+
+  part.split("").forEach((c) => {
+    if (c === "F") {
+      max -= Math.ceil((max - min) / 2);
+    } else {
+      min += Math.ceil((max - min) / 2);
+    }
+  });
+
+  return max;
+};
+
+export const determineCol = (part: string) => {
+  let min = 0;
+  let max = 7;
+
+  part.split("").forEach((c) => {
+    if (c === "L") {
+      max -= Math.ceil((max - min) / 2);
+    } else {
+      min += Math.ceil((max - min) / 2);
+    }
+  });
+
+  return max;
+};
+
+export const getSeatId = (boardingPass: string) => {
+  const match = boardingPass.match(PASS_MATCHER);
+  if (!match) return 0;
+  const [, rowInput, colInput] = match;
+  const row = determineRow(rowInput);
+  const col = determineCol(colInput);
+  return row * 8 + col;
+};
+
+export const findSeat = (ids: number[]) => {
+  const min = Math.min(...ids);
+  const max = Math.max(...ids);
+  for (let i = min; i < max; i += 1) {
+    if (!ids.includes(i)) return i;
+  }
+  return -1;
 };
