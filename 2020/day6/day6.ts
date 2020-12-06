2c723eeb8f9940660d6ebbb2719280db8e878a41
Alex Howard <thezanke@gmail.com>
2020-12-06 01:23:10 -0500

    use reduce for part 1

--- 2020/day6/day6.ts
+++ 2020/day6/day6.ts
@@ -1,12 +1,10 @@
 import { intersection } from "../common/arrays.ts";
 
-export const countUniqueAnswers = (groupAnswers: string[][]) => {
-  let total = 0;
-  groupAnswers.forEach((ga) => {
-    const set = new Set([...ga.join("")]);
-    total += set.size;
-  });
-  return total;
+export const countUniqueAnswers = (groups: string[][]) => {
+  return groups.reduce((total, groupForms) => {
+    const set = new Set([...groupForms.join("")]);
+    return total + set.size;
+  }, 0);
 };
 
 interface AnswerMap {
@@ -14,16 +12,16 @@ interface AnswerMap {
 }
 
 /* my original naive solution */
-export const countGroupedAnswers = (groupAnswers: string[][]) => {
+export const countGroupedAnswers = (groups: string[][]) => {
   let total = 0;
-  groupAnswers.forEach((ga) => {
+  groups.forEach((groupForms) => {
     const answerMap: AnswerMap = {};
-    const personCount = ga.length;
-    [...ga.join("")].forEach((l) => {
+    const totalForms = groupForms.length;
+    [...groupForms.join("")].forEach((l) => {
       answerMap[l] = answerMap[l] ? answerMap[l] + 1 : 1;
     });
     Object.values(answerMap).forEach((c) => {
-      if (c === personCount) total += 1;
+      if (c === totalForms) total += 1;
     });
   });
   return total;
