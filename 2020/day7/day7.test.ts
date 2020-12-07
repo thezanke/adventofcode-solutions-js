bd5da6f7e9d39a4d3f4e1a9ee828741c38e8007a
Alex Howard <thezanke@gmail.com>
2020-12-06 23:58:51 -0500

    stub day7

---
+++ 2020/day7/day7.test.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
+import { readInputFile } from "../common/readInputFile.ts";
+import {} from "./day7.ts";
+
+const exampleInput = readInputFile("day7/exampleInput.txt");
+const input = readInputFile("day7/input.txt");
+
+Deno.test("Day 7 - Part 1 - Example 1", () => {
+  assertEquals(false, true);
+});
+
+Deno.test("Day 7 - Part 1 - Answer", () => {
+  assertEquals(false, true);
+});
+
+Deno.test("Day 7 - Part 2 - Example 1", () => {
+  assertEquals(false, true);
+});
+
+Deno.test("Day 7 - Part 2 - Answer", () => {
+  assertEquals(false, true);
+});
