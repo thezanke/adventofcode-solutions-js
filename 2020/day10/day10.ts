3d5b008d38b21d5e3207c5fe60484c33d19a5ec1
Alex Howard <alex.howard@moserit.com>
2020-12-10 13:38:36 -0500

    clean

--- 2020/day10/day10.ts
+++ 2020/day10/day10.ts
@@ -3,13 +3,16 @@ import { memoize } from "../common/memoize.ts";
 export const countDifferences = (inputs: number[]) => {
   const sortedInput = [...inputs].sort((a, b) => a - b);
   const differences = [1, 3];
+
   return differences.map((d) => {
     let count = 0;
+
     sortedInput.forEach((a, i) => {
       const b = sortedInput[i + 1];
       if (!b) return;
       if (b - a === d) count += 1;
     });
+
     return count + 1;
   });
 };
@@ -18,16 +21,17 @@ export const createArrangementTree = (inputs: number[]) => {
   const max = Math.max(...inputs);
   const sortedInput = [0, ...inputs, max + 3].sort((a, b) => a - b);
   const tree: { [key: number]: number[] } = {};
+
   for (let i = 0; i < sortedInput.length; i += 1) {
     let a = sortedInput[i];
     if (!tree[a]) tree[a] = [];
+
     [1, 2, 3].forEach((d) => {
       const b = a + d;
-      if (sortedInput.includes(b)) {
-        tree[a].push(b);
-      }
+      if (sortedInput.includes(b)) tree[a].push(b);
     });
   }
+
   return tree;
 };
 
