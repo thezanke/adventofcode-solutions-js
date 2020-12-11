d3d95c91654f2dc8a23e005b44da8b2c0ab93316
Alex Howard <alex.howard@moserit.com>
2020-12-11 09:24:32 -0500

    starting part2, naive

--- 2020/day11/day11.test.ts
+++ 2020/day11/day11.test.ts
@@ -1,16 +1,26 @@
 import { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
 import { readInputFile } from "../common/readInputFile.ts";
-import { countType, findStableSeating1, Seat } from "./day11.ts";
+import {
+  countType,
+  findStableSeating1,
+  findStableSeating2,
+  Seat,
+} from "./day11.ts";
 
 const example = readInputFile("day11/exampleInput.txt").map((l) => l.split(""));
 const input = readInputFile("day11/input.txt").map((l) => l.split(""));
 
-Deno.test("Day 11 - Part 1 - Example 1", () => {
+Deno.test("Day 11 - Part 1 - Example", () => {
   const stableSeating = findStableSeating1(example);
   assertEquals(countType(stableSeating, Seat.occupied), 37);
 });
 
 Deno.test("Day 11 - Part 1 - Answer", () => {
   const stableSeating = findStableSeating1(input);
-  assertEquals(countType(stableSeating, Seat.occupied), 37);
+  assertEquals(countType(stableSeating, Seat.occupied), 2361);
 });
+
+// Deno.test("Day 11 - Part 2 - Example", () => {
+//   const stableSeating = findStableSeating2(example);
+//   assertEquals(countType(stableSeating, Seat.occupied), 37);
+// });
--- 2020/day11/day11.ts
+++ 2020/day11/day11.ts
@@ -1,7 +1,12 @@
 export enum Seat {
   empty = "L",
   occupied = "#",
+  missing = ".",
 }
+
+export const printChart = (chart: string[][]) => {
+  console.log(chart.map((r) => r.join("")).join("\n"));
+};
 
 export const findStableSeating1 = (initial: string[][]) => {
   let state = [...initial.map((r) => [...r])];
@@ -16,9 +21,7 @@ export const findStableSeating1 = (initial: string[][]) => {
       state[y + 1]?.[x],
       state[y + 1]?.[x - 1],
       state[y]?.[x - 1],
-    ].filter((p) => {
-      return p && p === type;
-    });
+    ].filter((p) => p === type);
   };
 
   let updated = true;
@@ -52,8 +55,6 @@ export const findStableSeating1 = (initial: string[][]) => {
     });
   }
 
-  console.log(state.map((r) => r.join("")).join("\n"));
-
   return state;
 };
 
@@ -64,4 +65,148 @@ export const countType = (chart: string[][], type: string) => {
       return t2;
     }, 0);
   }, 0);
+};
+
+export const findStableSeating2 = (initial: string[][]) => {
+  let state = [...initial.map((r) => [...r])];
+
+  const countAdjacent = (x: number, y: number, type: Seat) => {
+    const maxY = state.length - 1;
+    const maxX = state[0]?.length ?? 1 - 1;
+
+    if (!(maxY && maxX)) return 0;
+
+    const visible: string[] = [];
+
+    // up-left
+    if (y !== 0 && x !== 0) {
+      for (let cy = y - 1; cy >= 0; cy -= 1) {
+        for (let cx = x - 1; cx >= 0; cx -= 1) {
+          let pos = state[cy]?.[cx];
+          if (pos && pos !== Seat.missing) {
+            visible.push(pos);
+            break;
+          }
+        }
+      }
+    }
+
+    // up
+    if (y !== 0) {
+      for (let cy = y - 1; cy >= 0; cy -= 1) {
+        let pos = state[cy]?.[x];
+        if (pos && pos !== Seat.missing) {
+          visible.push(pos);
+          break;
+        }
+      }
+    }
+
+    // up-right
+    if (y !== 0 && x !== maxX) {
+      for (let cy = y - 1; cy >= 0; cy -= 1) {
+        for (let cx = x + 1; cx <= maxX; cx += 1) {
+          let pos = state[cy]?.[cx];
+          if (pos && pos !== Seat.missing) {
+            visible.push(pos);
+            break;
+          }
+        }
+      }
+    }
+
+    // right
+    if (x !== maxX) {
+      for (let cx = x + 1; cx <= maxX; cx += 1) {
+        let pos = state[y]?.[cx];
+        if (pos && pos !== Seat.missing) {
+          visible.push(pos);
+          break;
+        }
+      }
+    }
+
+    // down-right
+    if (y !== maxY && x !== maxX) {
+      for (let cy = y + 1; cy <= maxY; cy += 1) {
+        for (let cx = x + 1; cx <= maxX; cx += 1) {
+          let pos = state[cy]?.[cx];
+          if (pos && pos !== Seat.missing) {
+            visible.push(pos);
+            break;
+          }
+        }
+      }
+    }
+
+    // down
+    if (y !== maxY) {
+      for (let cy = y + 1; cy <= maxY; cy += 1) {
+        let pos = state[cy]?.[x];
+        if (pos && pos !== Seat.missing) {
+          visible.push(pos);
+          break;
+        }
+      }
+    }
+
+    // down-left
+    if (y !== maxY && x !== 0) {
+      for (let cy = y + 1; cy <= maxY; cy += 1) {
+        for (let cx = x - 1; cx >= 0; cx -= 1) {
+          let pos = state[cy]?.[cx];
+          if (pos && pos !== Seat.missing) {
+            visible.push(pos);
+            break;
+          }
+        }
+      }
+    }
+
+    // left
+    if (x !== 0) {
+      for (let cx = x - 1; cx >= 0; cx -= 1) {
+        let pos = state[y]?.[cx];
+        if (pos && pos !== Seat.missing) {
+          visible.push(pos);
+          break;
+        }
+      }
+    }
+
+    return visible.filter((p) => p === type).length;
+  };
+
+  let updated = true;
+
+  while (updated) {
+    updated = false;
+    state = state.map((marks, y) => {
+      return marks.map((mark, x) => {
+        if (mark !== Seat.empty && mark !== Seat.occupied) {
+          return mark;
+        }
+
+        const visibleOccupiedCount = countAdjacent(x, y, Seat.occupied);
+
+        if (mark === Seat.empty) {
+          if (!visibleOccupiedCount) {
+            updated = true;
+            return Seat.occupied;
+          }
+        }
+
+        if (mark === Seat.occupied) {
+          if (visibleOccupiedCount >= 5) {
+            updated = true;
+            return Seat.empty;
+          }
+        }
+
+        return mark;
+      });
+    });
+  }
+
+  return state;
 };
