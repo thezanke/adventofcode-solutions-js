600c5e86fb0e75ad51bd6800a9a87509e1a79c76
Alex Howard <thezanke@gmail.com>
2021-12-22 02:12:37 -0500

    comment & day22 pt1

--- 2021/day21/day21.js
+++ 2021/day21/day21.js
@@ -33,30 +33,36 @@ const rollPart2 = _.memoize(
   (p1, p2, s1, s2, c, r) => {
     let [, , _s1, _s2, _c, _r] = [p1, p2, s1, s2, c, r];
 
-    if (r === 3) {
-      _r = 1;
-      _c = 1 - c;
-      if (!c) _s1 += p1;
-      else _s2 += p2;
+    if (r === 3) {        // if we just finished the 3rd role
+      _r = 1;             // reset roll to 1 for the next game
+      _c = 1 - c;         // switch players for the next game
+      if (!c) _s1 += p1;  // if the current player was player 1, add to player 1's score
+      else _s2 += p2;     // otherwise add to player 2's score
     } else {
-      _r += 1;
+      _r += 1;            // on any other roll but 3, increment roll
     }
 
+    // player 1 has won this game
     if (_s1 >= 21) return [1, 0];
+
+    // player 2 has won this game
     if (_s2 >= 21) return [0, 1];
 
-    const winners = [0, 0];
+    // we haven't reach a win condition, initialize score buckets for child universe branches
+    const scores = [0, 0];
 
+    // perform the next "roll" (in all 3 child universe)
     [
       rollPart2(...addPosForPlayer(p1, p2, _c, 1), _s1, _s2, _c, _r),
       rollPart2(...addPosForPlayer(p1, p2, _c, 2), _s1, _s2, _c, _r),
       rollPart2(...addPosForPlayer(p1, p2, _c, 3), _s1, _s2, _c, _r),
     ].forEach((n) => {
-      winners[0] += n[0];
-      winners[1] += n[1];
+      // add the outcome of each child universe's roll to our scores
+      scores[0] += n[0];
+      scores[1] += n[1];
     });
 
-    return winners;
+    return scores;
   },
   (...args) => args.join(",")
 );
