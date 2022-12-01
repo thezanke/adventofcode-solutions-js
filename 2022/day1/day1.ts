becf22687236a1d3c5299ed2194e0f4e959771c2
Alex Howard <thezanke@gmail.com>
2022-12-01 00:18:52 -0500

    day1

--- 2022/day1/day1.ts
+++ 2022/day1/day1.ts
@@ -1,1 +1,12 @@
-export const day1 = () => true;
+const parseInput = (input: string) => input.split('\n\n').map(c => c.split('\n').map(Number).reduce((x, y) => x + y));
+
+export const part1 = (input: string) => {
+  const chunkValues = parseInput(input);
+  return Math.max(...chunkValues);
+};
+
+export const part2 = (input: string) => {
+  const chunkValues = parseInput(input).sort((a, b) => b - a);
+  const topChunks = chunkValues.slice(0, 3);
+  return topChunks.reduce((x, y) => x + y);
+};
