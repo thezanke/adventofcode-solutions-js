3411c9323a9421d526629bcfc51949b064acb470
Alex Howard <thezanke@gmail.com>
2022-12-04 13:49:20 -0500

    better terms

--- 2022/day4/day4.ts
+++ 2022/day4/day4.ts
@@ -5,15 +5,15 @@ type NumberRanges = [number, number, number, number];
 const parseNumberRangesFromInput = (input: string) => input.split('\n').map(p => p.split(/,|-/).map(Number).flat() as NumberRanges);
 
 export const part1 = (input: string): number => {
-  return parseNumberRangesFromInput(input).reduce((t, [a, b, c, d]) => {
-    const hasContainment = (a <= c && b >= d) || (c <= a && d >= b);
+  return parseNumberRangesFromInput(input).reduce((t, [a, b, x, y]) => {
+    const hasContainment = (a <= x && b >= y) || (x <= a && y >= b);
     return hasContainment ? t + 1 : t;
   }, 0);
 };
 
 export const part2 = (input: string) => {
-  return parseNumberRangesFromInput(input).reduce((t, [a, b, c, d]) => {
-    const hasOverlap = (a <= d && b >= c) || (c >= b && d <= a);
+  return parseNumberRangesFromInput(input).reduce((t, [a, b, x, y]) => {
+    const hasOverlap = (a <= y && b >= x) || (x >= b && y <= a);
     return hasOverlap ? t + 1 : t;
   }, 0);
 };
