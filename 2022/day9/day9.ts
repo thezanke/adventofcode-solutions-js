93e39d2df9f214d1898e942af949cbc3481ad3ef
Alex Howard <thezanke@gmail.com>
2022-12-09 11:36:37 -0500

    clean

--- 2022/day9/day9.ts
+++ 2022/day9/day9.ts
@@ -25,7 +25,7 @@ const parseInput = (input: string): Array<[Vec2d, number]> => {
 
 type Positions = Vec2d[]
 
-const determineMove = (positions: Positions): Vec2d => {
+const determineSegmentMove = (positions: Positions): Vec2d => {
   const [last, current] = positions
 
   const displacementVector = vecUtils.subtract(last, current) as Vec2d
@@ -43,25 +43,26 @@ const determineMove = (positions: Positions): Vec2d => {
 
 const vecToKey = (vec: number[]): string => vec.join(',')
 
-export const part1 = (input: string, length: number = 2): number => {
+export const solve = (input: string, length: number = 2): number => {
+  const moves = parseInput(input)
   const positions: Positions = Array.from({ length }, () => [...START])
-  const tail = positions[positions.length - 1]
+  const [tail] = positions.slice(-1)
   const visited = new Set([vecToKey(START)])
 
-  const headMoves = parseInput(input)
-
-  for (const [vec, dist] of headMoves) {
+  for (const [vec, dist] of moves) {
     for (let d = 0; d < dist; d += 1) {
       const [head] = positions
       vecUtils.add(head, vec, true)
 
       for (let p = 1; p < positions.length; p += 1) {
-        const [last, current] = positions.slice(p - 1)
-        const move = determineMove([last, current])
-        const isStalled = vecUtils.sum(move.map(Math.abs)) === 0
+        const [prevSegment, currSegment] = positions.slice(p - 1)
+        const segmentMove = determineSegmentMove([prevSegment, currSegment])
+
+        const isStalled = vecUtils.sum(segmentMove.map(Math.abs)) === 0
         if (isStalled) break
-        vecUtils.add(current, move, true)
-        if (current === tail) visited.add(vecToKey(current))
+
+        vecUtils.add(currSegment, segmentMove, true)
+        if (currSegment === tail) visited.add(vecToKey(currSegment))
       }
     }
   }
@@ -69,6 +70,5 @@ export const part1 = (input: string, length: number = 2): number => {
   return visited.size
 }
 
-export const part2 = (input: string): number => {
-  return part1(input, 10)
-}
+export const part1 = (input: string): number => solve(input)
+export const part2 = (input: string): number => solve(input, 10)
