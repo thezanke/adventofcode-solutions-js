269d5ebe69425c533d5772a03999696773a04d11
Alex Howard <thezanke@gmail.com>
2022-12-11 02:02:48 -0500

    tweak

--- 2022/day11/day11.ts
+++ 2022/day11/day11.ts
@@ -1,8 +1,8 @@
 class Monkey {
   id: number
   items: number[]
-  opString: string
-  tester: [divBy: number, ifTrue: number, ifFalse: number]
+  operation: [sign: string, valStr: string]
+  tester: [testVal: number, trueMonkey: number, falseMonkey: number]
   inspections = 0
 
   constructor (monkeyLines: string[], i: number) {
@@ -13,23 +13,45 @@ class Monkey {
     this.items = itemsString.split(', ').map(Number)
 
     const [,opString] = opLine.split(': ')
-    this.opString = opString
+    const [sign, valStr] = opString.split(' ').slice(-2)
+    this.operation = [sign, valStr]
 
     this.tester = testLines.map(line => {
-      const [last] = line.split(' ').splice(-1)
+      const [last] = line.split(' ').slice(-1)
       return Number(last)
     }) as Monkey['tester']
   }
 
-  takeTurn () {}
+  inspectNextItem (monkeys: Monkey[]): void {
+    this.inspections += 1
+    // eslint-disable-next-line prefer-const, @typescript-eslint/no-non-null-assertion
+    let itemVal = this.items.shift()!
+    const [sign, valStr] = this.operation
+    const val = valStr === 'old' ? itemVal : Number(valStr)
+    // eslint-disable-next-line no-eval
+    itemVal = eval(`itemVal ${sign} ${val}`)
+    itemVal = Math.floor(itemVal / 3)
+    const [testVal, trueMonkey, falseMonkey] = this.tester
+    const receivingMonkey = itemVal % testVal === 0 ? trueMonkey : falseMonkey
+    monkeys[receivingMonkey]?.items.push(itemVal)
+  }
+
+  takeTurn (monkeys: Monkey[]): void {
+    const itemCount = this.items.length
+    if (itemCount === 0) return
+    for (let i = 0; i < itemCount; i += 1) {
+      this.inspectNextItem(monkeys)
+    }
+  }
 }
 
-const parseInput = (input: string): Monkey[] => {
-  return input.split('\n\n').map((c, i) => new Monkey(c.split('\n'), i))
+const parseInput = (input: string, isBig = false): Array<Monkey | BigMonkey> => {
+  const MonkeyClass = isBig ? BigMonkey : Monkey
+  return input.split('\n\n').map((c, i) => new MonkeyClass(c.split('\n'), i))
 }
 
 export const part1 = (input: string): number => {
-  const monkeys = parseInput(input)
+  const monkeys = parseInput(input) as Monkey[]
 
   for (let i = 0; i < 20; i += 1) {
     for (const monkey of monkeys) {
@@ -37,7 +59,74 @@ export const part1 = (input: string): number => {
     }
   }
 
-  return -1
+  const [x, y] = monkeys.map(({ inspections }) => inspections).sort((a, b) => b - a)
+
+  return x * y
 }
 
-export const part2 = (input: string): number => -1
+class BigMonkey {
+  id: number
+  items: bigint[]
+  operation: [sign: string, valStr: string]
+  tester: [testVal: bigint, trueMonkey: number, falseMonkey: number]
+  inspections = 0
+
+  constructor (monkeyLines: string[], i: number) {
+    this.id = i
+    const [,itemLine, opLine, ...testLines] = monkeyLines
+
+    const [,itemsString] = itemLine.split(': ')
+    this.items = itemsString.split(', ').map(BigInt)
+
+    const [,opString] = opLine.split(': ')
+    const [sign, valStr] = opString.split(' ').slice(-2)
+    this.operation = [sign, valStr]
+
+    const [testVal, trueMonkey, falseMonkey] = testLines.map(line => {
+      const [last] = line.split(' ').slice(-1)
+      return Number(last)
+    }) as Monkey['tester']
+
+    this.tester = [BigInt(testVal), trueMonkey, falseMonkey]
+  }
+
+  inspectNextItem (monkeys: BigMonkey[]): void {
+    this.inspections += 1
+    // eslint-disable-next-line prefer-const, @typescript-eslint/no-non-null-assertion
+    let itemVal = this.items.shift()!
+    const [sign, opValStr] = this.operation
+    const opVal = opValStr === 'old' ? itemVal : BigInt(opValStr)
+    if (sign === '+') {
+      itemVal += opVal
+    } else if (sign === '*') {
+      itemVal *= opVal
+    }
+    // eslint-disable-next-line no-eval
+    const [testVal, trueMonkey, falseMonkey] = this.tester
+    const receivingMonkey = itemVal % testVal === 0n ? trueMonkey : falseMonkey
+    monkeys[receivingMonkey]?.items.push(itemVal)
+  }
+
+  takeTurn (monkeys: BigMonkey[]): void {
+    const itemCount = this.items.length
+    if (itemCount === 0) return
+    for (let i = 0; i < itemCount; i += 1) {
+      this.inspectNextItem(monkeys)
+    }
+  }
+}
+
+export const part2 = (input: string): number => {
+  const monkeys = parseInput(input, true) as BigMonkey[]
+
+  for (let i = 0; i < 10000; i += 1) {
+    console.log(`~ Round ${i + 1} ~`)
+    for (const monkey of monkeys) {
+      monkey.takeTurn(monkeys)
+    }
+  }
+
+  const [x, y] = monkeys.map(({ inspections }) => inspections).sort((a, b) => b - a)
+
+  return x * y
+}
